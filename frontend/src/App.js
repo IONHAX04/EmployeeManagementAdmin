80ef97a5213e680e9e3347aa7d1762890c4bbd54
IONHAX04 <thirukumara04@gmail.com>
2024-07-28 09:56:00 +0530

    Latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Nav from "./Components/00-Navbar/Nav";
+import Dashboard from "./Components/01-Dashboard/Dashboard";
+
+
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>a
-      </header>
+      <Router>
+        <Nav>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </Nav>
+      </Router>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "./flag.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,6 +10,7 @@ import { PrimeReactProvider } from "primereact/api";
 import "primeflex/primeflex.css";
 import "primereact/resources/primereact.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
