7146051587e93eab9f905be82ea463152ab4948c
IONHAX04 <thirukumara04@gmail.com>
2024-07-28 16:38:50 +0530

    Dashboard UI Completed

--- frontend/src/Components/00-Navbar/Nav.js
+++ frontend/src/Components/00-Navbar/Nav.js
@@ -1,7 +1,130 @@
-import React from 'react'
+import { useState } from "react";
 
-export default function Nav() {
+import { AnimatePresence, motion } from "framer-motion";
+import { FaBars, FaUserCircle, FaExchangeAlt } from "react-icons/fa";
+import { MdDashboard, MdSettings } from "react-icons/md";
+import { ImExit } from "react-icons/im";
+import { RiOrganizationChart } from "react-icons/ri";
+
+import { NavLink } from "react-router-dom";
+
+import "./nav.css";
+
+const routes = [
+  {
+    path: "/",
+    name: "Dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    path: "/employee",
+    name: "Employee",
+    icon: <FaUserCircle />,
+  },
+  {
+    path: "/manage",
+    name: "Manage Access",
+    icon: <FaExchangeAlt />,
+  },
+  {
+    path: "/orgChart",
+    name: "Organization",
+    icon: <RiOrganizationChart />,
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    icon: <MdSettings />,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    icon: <ImExit />,
+  },
+];
+
+export default function Nav({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const showAnimation = {
+    hidden: {
+      width: 0,
+      opacity: 0,
+      transition: {
+        duration: 0.2,
+      },
+    },
+    show: {
+      width: "auto",
+      opacity: 1,
+      transition: {
+        duration: 0.2,
+      },
+    },
+  };
+
   return (
-    <div>Nav</div>
-  )
+    <div>
+      <div className="main_container">
+        <motion.div
+          animate={{
+            width: isOpen ? "250px" : "60px",
+            transition: {
+              duration: 0.2,
+              type: "spring",
+              damping: 10,
+            },
+          }}
+          className="sidebar"
+        >
+          <div className="top_section">
+            <AnimatePresence>
+              {isOpen && (
+                <motion.h1
+                  className="logo"
+                  variants={showAnimation}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                >
+                  Admin Panel
+                </motion.h1>
+              )}
+            </AnimatePresence>
+            <div className="bars">
+              <FaBars onClick={toggle} />
+            </div>
+          </div>
+
+          <section className="routes">
+            {routes.map((route) => (
+              <NavLink
+                to={route.path}
+                key={route.name}
+                className="link"
+                activeClassName="active"
+              >
+                <div className="icon">{route.icon}</div>
+                <AnimatePresence>
+                  {isOpen && (
+                    <motion.div
+                      className="link_text"
+                      variants={showAnimation}
+                      initial="hidden"
+                      animate="show"
+                      exit="hidden"
+                    >
+                      {route.name}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </NavLink>
+            ))}
+          </section>
+        </motion.div>
+        <main style={{ width: isOpen ? "85vw" : "95vw" }}>{children}</main>
+      </div>
+    </div>
+  );
 }
--- frontend/src/Components/01-Dashboard/Dashboard.js
+++ frontend/src/Components/01-Dashboard/Dashboard.js
@@ -1,7 +1,205 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { Divider, Fieldset, Knob, Avatar, AvatarGroup } from "primereact";
+
+import profileImage from "../../Assets/icons/boy.png";
+import coverImage from "../../Assets/images/cover.png";
+import holidayImage from "../../Assets/images/holiday.png";
+import "./dashboard.css";
 
 export default function Dashboard() {
+  const [attendacenPercentage, setAttendancePercentage] = useState(90);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <div className="primaryNav">
+        <p>Dashboard</p>
+        <p className="p-last-edit">Profile last updated 11:43 | Jul 28 2024</p>
+      </div>
+
+      <div className="contents">
+        <div className="userProfile">
+          <div className="coverImage">
+            <img src={coverImage} alt="coverImage" />
+          </div>
+          <div className="coverContents">
+            <img src={profileImage} alt="userProfile" />
+            <div className="userDetails">
+              <div className="userDetOne">
+                <div className="userDetPrimary">
+                  <p className="username">User name</p>
+                  <p className="useremail">User email</p>
+                </div>
+                <p className="empPosition">Position</p>
+              </div>
+              <div className="userDetTwo">
+                <p><span>Employee ID </span>: COMP01HR1001</p>
+                <Divider layout="vertical" />
+                <p><span>Department </span>: HR</p>
+                <Divider layout="vertical" />
+                <p><span>Mobile </span>: +91 9933994499</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="userDashboard grid-container">
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Your Actions">
+                <div className="actions">
+                  <p>
+                    <span>0 </span> actions left
+                  </p>
+                  <p>View Your Previous Actions</p>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Attendance">
+                <div className="attendance">
+                  <Knob
+                    value={attendacenPercentage}
+                    onChange={(e) =>
+                      setAttendancePercentage(e.attendacenPercentage)
+                    }
+                    valueTemplate={"{value}%"}
+                    size={120}
+                  />
+                  <div className="avgWorkHrs">
+                    <p>
+                      <span>7.5 Hrs</span> Per Day
+                    </p>
+                    <p>* Avg Working Hours</p>
+                  </div>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Leave Balance">
+                <div className="leaveBalance">
+                  <div className="consumed">
+                    <p>5</p>
+                    <p>Consumed</p>
+                  </div>
+                  <div className="balance">
+                    <p>10</p>
+                    <p>Balance</p>
+                  </div>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Out Today">
+                <div className="card outToday flex justify-content-center">
+                  <AvatarGroup>
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/asiyajavayant.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/onyamalimba.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/ionibowcher.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/xuxuefeng.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar label="+2" shape="circle" size="large" />
+                  </AvatarGroup>
+                  <p>2 People from HR out today</p>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Upcoming Out">
+                <div className="card outToday flex justify-content-center">
+                  <AvatarGroup>
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/asiyajavayant.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/onyamalimba.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/ionibowcher.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar
+                      image="https://primefaces.org/cdn/primereact/images/avatar/xuxuefeng.png"
+                      size="large"
+                      shape="circle"
+                    />
+                    <Avatar label="+2" shape="circle" size="large" />
+                  </AvatarGroup>
+                  <p>3 People from Sales</p>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+
+          <div className="grid-item">
+            <div className="card">
+              <Fieldset legend="Next Holiday">
+                <div className="holiday">
+                  <div className="holidayContents">
+                    <div className="date">
+                      <div className="dateOne">15</div>
+                      <div className="dateTwo">
+                        <p>Aug</p>
+                        <p>2024</p>
+                      </div>
+                    </div>
+                    <img
+                      src={holidayImage}
+                      alt="Holiday"
+                      height={80}
+                      width={80}
+                    />
+                  </div>
+                  <p>Independence Day</p>
+                </div>
+              </Fieldset>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
